4115cac5403b224468392d6519c0bd4ed4d1713b
fngoc <vitmarkha@gmail.com>
2024-07-06 13:07:22 +0300

    add iter5

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type flags struct {
 	ServerAddress     string
@@ -13,4 +16,14 @@ func ParseArgs() {
 	flag.StringVar(&Flags.ServerAddress, "a", "localhost:8080", "server address")
 	flag.StringVar(&Flags.BaseResultAddress, "b", "http://localhost:8080", "base result server address")
 	flag.Parse()
+
+	serverAddressEnv, findAddress := os.LookupEnv("SERVER_ADDRESS")
+	serverBaseUrlEnv, findBaseUrl := os.LookupEnv("BASE_URL")
+
+	if findAddress {
+		Flags.ServerAddress = serverAddressEnv
+	}
+	if findBaseUrl {
+		Flags.BaseResultAddress = serverBaseUrlEnv
+	}
 }
