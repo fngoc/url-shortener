ba55edbaea25c092e1e667253e3f0772759e3fcf
fngoc <vitmarkha@gmail.com>
2024-07-28 15:56:15 +0300

    add iter8 in sprint 2

--- cmd/shortener/handlers/handlers.go
+++ cmd/shortener/handlers/handlers.go
@@ -33,7 +33,8 @@ func GetRedirectWebhook(w http.ResponseWriter, r *http.Request) {
 
 // PostSaveWebhook функция обработчик POST HTTP-запроса
 func PostSaveWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost || !strings.Contains(r.Header.Get("Content-Type"), "text/plain") {
+	if r.Method != http.MethodPost || (!strings.Contains(r.Header.Get("Content-Type"), "text/plain") &&
+		!strings.Contains(r.Header.Get("Content-Type"), "application/x-gzip")) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
