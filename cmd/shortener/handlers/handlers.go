4a132d74e4e052ea4fd1f87d62dc4dd19d857b9e
fngoc <vitmarkha@gmail.com>
2024-07-28 17:14:39 +0300

    add iter8 in sprint 2

--- cmd/shortener/handlers/handlers.go
+++ cmd/shortener/handlers/handlers.go
@@ -34,7 +34,9 @@ func GetRedirectWebhook(w http.ResponseWriter, r *http.Request) {
 
 // PostSaveWebhook функция обработчик POST HTTP-запроса
 func PostSaveWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost || (!strings.Contains(r.Header.Get("Content-Type"), "text/plain")) {
+	allowedTextPlan := !strings.Contains(r.Header.Get("Content-Type"), "text/plain")
+	gzipTextPlan := !strings.Contains(r.Header.Get("Content-Type"), "gzip")
+	if r.Method != http.MethodPost || !allowedTextPlan || !gzipTextPlan {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -59,7 +61,9 @@ func PostSaveWebhook(w http.ResponseWriter, r *http.Request) {
 
 // PostShortenWebhook функция обработчик POST HTTP-запроса
 func PostShortenWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost || !strings.Contains(r.Header.Get("Content-Type"), "application/json") {
+	allowedApplicationJson := strings.Contains(r.Header.Get("Content-Type"), "application/json")
+	gzipTextPlan := !strings.Contains(r.Header.Get("Content-Type"), "gzip")
+	if r.Method != http.MethodPost || !allowedApplicationJson || !gzipTextPlan {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
