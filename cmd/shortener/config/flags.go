51420976bdd34e6578a93ba5628f48a16c039ac0
fngoc <vitmarkha@gmail.com>
2024-08-07 21:10:25 +0300

    add iter10 in sprint 3

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -9,6 +9,7 @@ type flags struct {
 	ServerAddress     string
 	BaseResultAddress string
 	FilePath          string
+	DBConf            string
 }
 
 var Flags flags
@@ -17,11 +18,13 @@ func ParseArgs() {
 	flag.StringVar(&Flags.ServerAddress, "a", "localhost:8080", "server address")
 	flag.StringVar(&Flags.BaseResultAddress, "b", "http://localhost:8080", "base result server address")
 	flag.StringVar(&Flags.FilePath, "f", "data.json", "file path")
+	flag.StringVar(&Flags.DBConf, "d", "host=localhost user=postgres password=postgres dbname=gpb_db sslmode=disable", "db params")
 	flag.Parse()
 
 	serverAddressEnv, findAddress := os.LookupEnv("SERVER_ADDRESS")
 	serverBaseURLEnv, findBaseURL := os.LookupEnv("BASE_URL")
 	filePathEnv, findFilePath := os.LookupEnv("FILE_STORAGE_PATH")
+	DBEnv, findDBConf := os.LookupEnv("DATABASE_DSN")
 
 	if findAddress {
 		Flags.ServerAddress = serverAddressEnv
@@ -31,5 +34,8 @@ func ParseArgs() {
 	}
 	if findFilePath {
 		Flags.FilePath = filePathEnv
+	}
+	if findDBConf {
+		Flags.DBConf = DBEnv
 	}
 }
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/fngoc/url-shortener/cmd/shortener/config"
+	"github.com/fngoc/url-shortener/cmd/shortener/database"
 	"github.com/fngoc/url-shortener/cmd/shortener/server"
 	"github.com/fngoc/url-shortener/cmd/shortener/storage"
 )
@@ -11,6 +12,9 @@ func main() {
 	config.ParseArgs()
 
 	if err := storage.InitializeLocalStore(config.Flags.FilePath); err != nil {
+		panic(err)
+	}
+	if err := database.InitializeDB(config.Flags.DBConf); err != nil {
 		panic(err)
 	}
 	if err := server.Run(); err != nil {
