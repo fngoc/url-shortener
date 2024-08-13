bf8fc980f3637e13a40dcba4883bab18e981ae7a
fngoc <vitmarkha@gmail.com>
2024-08-13 17:03:34 +0300

    add iter13 in sprint 3

--- cmd/shortener/storage/storage_test.go
+++ cmd/shortener/storage/storage_test.go
@@ -45,12 +45,12 @@ func TestLocalStore_GetData(t *testing.T) {
 		t.Fatal(err)
 	}
 	mockLocalStore := make(LocalStore)
-	require.NoError(t, mockLocalStore.SaveData("key", "value"))
-	require.NoError(t, mockLocalStore.SaveData("vdsdhhmggdsadcxvvfsdsaf", "fdsbhgkjmdfsaew341gfds"))
+	require.NoError(t, mockLocalStore.SaveData(nil, "key", "value"))
+	require.NoError(t, mockLocalStore.SaveData(nil, "vdsdhhmggdsadcxvvfsdsaf", "fdsbhgkjmdfsaew341gfds"))
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			value, err := mockLocalStore.GetData(tt.input)
+			value, err := mockLocalStore.GetData(nil, tt.input)
 			if tt.want.isError {
 				require.Error(t, err)
 			}
@@ -105,12 +105,12 @@ func TestLocalStore_SaveData(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := mockLocalStore.SaveData(tt.inputKey, tt.inputValue)
+			err := mockLocalStore.SaveData(nil, tt.inputKey, tt.inputValue)
 			if tt.isError {
 				require.Error(t, err)
 				return
 			}
-			value, err := mockLocalStore.GetData(tt.inputKey)
+			value, err := mockLocalStore.GetData(nil, tt.inputKey)
 			if err != nil {
 				t.Fatal(err)
 			}
