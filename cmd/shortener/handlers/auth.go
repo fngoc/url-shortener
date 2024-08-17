cb8d446544980eaa6c7c1c76c2b3f1fc6b2c1219
fngoc <vitmarkha@gmail.com>
2024-08-17 21:15:17 +0300

    add iter14 in sprint 4

--- cmd/shortener/handlers/auth.go
+++ cmd/shortener/handlers/auth.go
@@ -80,7 +80,7 @@ func AuthMiddleware(h http.HandlerFunc) http.HandlerFunc {
 				HttpOnly: true,
 				Secure:   true,
 			})
-			w.Header().Set("Authorization", "Bearer "+token)
+			w.Header().Set("Authorization", token)
 			logger.Log.Info(fmt.Sprintf("Create new cookie with token: %s for %s", token, r.URL.Path))
 		}
 		h.ServeHTTP(w, r)
