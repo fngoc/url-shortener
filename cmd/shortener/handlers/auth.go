e3d57c6e172aab7bef1964ea5bd7c9ecaba3566e
fngoc <vitmarkha@gmail.com>
2024-08-29 22:23:26 +0300

    add iter15 in sprint 4

--- cmd/shortener/handlers/auth.go
+++ cmd/shortener/handlers/auth.go
@@ -81,7 +81,7 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 				Path:     "/",
 				MaxAge:   3600,
 				HttpOnly: true,
-				Secure:   false,
+				Secure:   true,
 			})
 			authCtx = context.WithValue(r.Context(), constants.UserIDKey, GetUserID(token))
 			w.Header().Set("Authorization", token)
